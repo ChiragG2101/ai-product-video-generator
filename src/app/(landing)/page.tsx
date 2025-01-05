eed57ee6662b8f6d9d5df766b1cad0b293be1e85
Chirag Gupta <chiragg593@gmail.com>
2025-01-05 23:27:54 +0530

    base change

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen">
-      <Button color="primary">This is working</Button>
+    <div className="w-full min-h-screen flex  items-center justify-center">
+      <Button color="primary">Hello World</Button>
     </div>
   );
 }
